01c580e9914cf48a506611e2d425618ce72c5da2
Rizwan Shah <98143667+rizwanshah123@users.noreply.github.com>
2023-11-18 11:34:52 +0500

    basic_variables.dart

---
+++ basic_variables.dart
@@ -0,0 +1,35 @@
+
+void main() {
+  //Using Print Method
+  print("Hello world ");
+  
+  print("This is my first Program in Dart ");
+  
+  // declare Variable
+  int number;
+
+  //initilization of variable
+ int number1=3;
+  
+  String sentance="this is my sentance";
+ 
+  bool check=true;
+  
+  var a="this is string";
+ 
+  double num=12.76;
+  
+  List t=[];
+  Map map={};
+  
+  int add = sum();
+  print(check);
+  //Using Standred Input/Output
+  //stdin.write("")
+}
+
+
+int sum() {
+  
+  return 7+3;
+}
